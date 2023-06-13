ade31d9ed78ae06a7c27583cae533f46add445f9
Kirill Starodubsev <colereteps@gmail.com>
2023-06-13 19:39:30 +0400

    chore: Add distribution of statics

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,14 @@
 import express from 'express';
-
+import path from 'path';
 const app = express();
 const port = 3100;
+const clientDistPath = path.resolve(__dirname, '../../client/dist');
+
+app.use(express.static(clientDistPath));
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.sendFile(path.join(clientDistPath, 'index.html'));
+});
 
 app.listen(port, () => {
-    console.log('Server started');
+    console.log(`Server is running on ${port}`);
 });
