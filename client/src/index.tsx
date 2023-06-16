25da7a2514674e4995127e9058d72c16b3a9bf63
Kirill Starodubsev <colereteps@gmail.com>
2023-06-16 19:35:48 +0400

    feat: Add bootstrap

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
     <BrowserRouter>
